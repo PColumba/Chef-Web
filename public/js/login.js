c31dfc606eb8b9f72ab386ce935611f20a1f9335
PColumba <pawel.agolab@gmail.com>
2019-06-06 14:06:23 +0200

    Added facebook login. Added back button. Added client-side c

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,24 @@
 //sign up and register users
 
+function signInWithFacebook() {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+       router.navigateTo("/search"); 
+    }).catch(function(error) {
+      alert("Ups something went wrong: " + error.message);
+    });
+}
+
 function register(){
     
     const email = document.getElementById("loginForm").elements[0].value;
     const password = document.getElementById("loginForm").elements[1].value;
-   
+    
+    if(email == "" || password == ""){
+        alert("Please fill out both email and password field")
+        return;
+    }    
+        
     firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user){
       alert("Your account has been registered, please Sign In to continue");
     }).catch(function(error) {
@@ -17,8 +31,11 @@ function signIn(){
     const email = document.getElementById("loginForm").elements[0].value;
     const password = document.getElementById("loginForm").elements[1].value;
     
+    if(email == "" || password == ""){
+        alert("Please fill out both email and password field")
+        return;
+    }
     firebase.auth().signInWithEmailAndPassword(email, password).then(function(user){
-        $('#user-info').text(user.user.email);
         router.navigateTo("/search");
     }).catch(function(error) {
       alert("Ups something went wrong: " + error.message);
